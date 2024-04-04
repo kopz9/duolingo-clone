1c9531de8e4ca3dcd0200b952867ee894b7e8607
kopz9 <gustavoeduardo438@gmail.com>
2024-04-04 06:17:29 -0300

    feat: add clerk authentication

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import {authMiddleware} from '@clerk/nextjs'
+
+export default authMiddleware({
+  publicRoutes: ['/'],
+});
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
