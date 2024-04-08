c298649440696ebaaf88d0fb2ce7a945983cb2f1
kopz9 <gustavoeduardo438@gmail.com>
2024-04-07 23:12:51 -0300

    feat: update sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,7 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
 
 type Props = {
   className?: string;
@@ -8,11 +11,39 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem
+          label="Learn"
+          iconSrc="/learn.svg"
+          href="/learn" />
+        <SidebarItem
+          label="Leaderboard"
+          iconSrc="/leaderboard.svg"
+          href="/leaderboard"
+        />
+        <SidebarItem 
+          label="Quests"
+          iconSrc="/quests.svg" 
+          href="/quests"
+        />
+        <SidebarItem 
+          label="Shop"
+          iconSrc="/shop.svg"
+          href="/shop"
+        />
+      </div>
     </div>
   );
 };
